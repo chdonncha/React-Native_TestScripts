5b00eeb0701465eb08f7948e6c82356b9979e150
chdonncha <chdonncha@gmail.com>
2016-04-22 15:18:37 +0100

    Code Cleanup

--- App/Components/Sproose_TestApp/Buy_View.js
+++ App/Components/Sproose_TestApp/Buy_View.js
@@ -18,34 +18,12 @@ const {
   Image
  } = React;
 
-var testData =
-[{"title":"Shirts"},
-{"title":"Jeans"},
-{"title":"Suits"},
-{"title":"Tux"},
-{"title":"Homeclothes"},
-{"title":"Image1"},
-{"title":"Image1"},
-{"title":"Image1"},
-{"title":"Image1"},
-{"title":"Image1"},
-{"title":"Image1"}];
-
 const Firebase = require('firebase');
 const styles = require('../../../styles');
 
 var _scrollView: ScrollView;
 
 class Buy_View extends React.Component{
-
-	/*backToMenu()
-	{
-		//var navigator = this.props.navigator;
-		this.props.navigator.pop({
-        id: 'Main',
-    	});
-	}*/
-
 	constructor(props) {
 	super(props);
 	this.itemsRef = new Firebase("https://luminous-inferno-2855.firebaseio.com/items");
@@ -57,75 +35,19 @@ class Buy_View extends React.Component{
 	};
 }
 
-listenForItems(itemsRef) {
-  itemsRef.on('value', (snap) => {
-
-    // get children as an array
-    var items = [];
-    snap.forEach((child) => {
-      items.push({
-        title: child.val().title,
-        subTitle: child.val().subTitle,
-        _key: child.key()
-      });
-    });
-
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(items)
-    });
-  });
-}
-
-_addItem() {
-  Alert.alert(
-    'Add New Item',
-    null,
-    [
-      {
-        text: 'Add',
-        onPress: (text) => {
-          this.itemsRef.push({ title: text })
-        }
-      },
-    ],
-    'plain-text'
-  );
-}
-
-	componentDidMount() {
-  this.setState({
-    dataSource: this.state.dataSource.cloneWithRows(testData)
-  })
-  //this.listenForItems(this.itemsRef);
-}
-
-  _renderItem(item) {
-      const onPress = () => {
-        Alert.alert(
-          'Complete',
-          null,
-          [
-            {text: 'Complete', onPress: (text) => this.itemsRef.child(item._key).remove()},
-            {text: 'Cancel', onPress: (text) => console.log('Cancel')}
-          ],
-          'default'
-        );
-      };
-      return (
-        <ListItem item={item} onPress={onPress} />
-    );
-  }
-
   bSignFacebook() {
     console.log('clicked signup facebook');
     this.props.navigator.push({
-        id: 'SignUpFB',
+        id: 'Main',
       });
   }
 
-  render() {
+renderTitle() {
     return (
-      <View>
+    <View
+      style={styles.container}>
+      <TouchableOpacity
+        onPress={this.bSignFacebook.bind(this)}>
           <Image source={
               require('./Images/Sproose_Black.png')}
               style={{
@@ -133,55 +55,69 @@ _addItem() {
                 width: 125,
                 marginTop: 30,
                 marginBottom: 30,
-                marginLeft: 115,
               }}
               resizeMode={'stretch'}>
           </Image>
-        <ScrollView contentContainerStyle={styles.contentContainer}
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
-          ref={(ScrollView) => {_scrollView = ScrollView; }}
-          automaticallyAdjustContentInsets={false}
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          style={[styles.scrollView, styles.horizontalScrollView]}>
+renderNavBar() {
+  return(
+    <ScrollView contentContainerStyle={styles.contentContainer}
 
-            <TouchableOpacity
-              style={styles.scrollButton}
-              onPress={this.bSignFacebook.bind(this)}>
+      ref={(ScrollView) => {_scrollView = ScrollView; }}
+      automaticallyAdjustContentInsets={false}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+      style={[styles.scrollView, styles.horizontalScrollView]}>
 
-              <Text style={styles.scrollHorizontalText}>
-                SERVICE WASH
-              </Text>
+        <TouchableOpacity
+          style={styles.scrollButton}
+          onPress={this.bSignFacebook.bind(this)}>
 
-            </TouchableOpacity>
+          <Text style={styles.scrollHorizontalText}>
+            SERVICE WASH
+          </Text>
 
-            <TouchableOpacity
-              style={styles.scrollButton}
-              onPress={this.bSignFacebook.bind(this)}>
+        </TouchableOpacity>
 
-              <Text style={styles.scrollHorizontalText}>
-                DRY CLEANING
-              </Text>
-            </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.scrollButton}
+          onPress={this.bSignFacebook.bind(this)}>
 
-            <TouchableOpacity
-              style={styles.scrollButton}
-              onPress={this.bSignFacebook.bind(this)}>
+          <Text style={styles.scrollHorizontalText}>
+            DRY CLEANING
+          </Text>
+        </TouchableOpacity>
 
-              <Text style={styles.scrollHorizontalText}>
-                HOUSEHOLD
-              </Text>
-            </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.scrollButton}
+          onPress={this.bSignFacebook.bind(this)}>
 
-            <TouchableOpacity
-              style={styles.scrollButton}
-              onPress={this.bSignFacebook.bind(this)}>
+          <Text style={styles.scrollHorizontalText}>
+            HOUSEHOLD
+          </Text>
+        </TouchableOpacity>
 
-              <Text style={styles.scrollHorizontalText}>
-                OTHER
-              </Text>
-            </TouchableOpacity>
-        </ScrollView>
+        <TouchableOpacity
+          style={styles.scrollButton}
+          onPress={this.bSignFacebook.bind(this)}>
+
+          <Text style={styles.scrollHorizontalText}>
+            OTHER
+          </Text>
+        </TouchableOpacity>
+    </ScrollView>
+  );
+}
+
+  render() {
+    return (
+      <View>
+        {this.renderTitle()}
+        {this.renderNavBar()}
 
         <Image source={
           require('./Images/line.jpg')}
